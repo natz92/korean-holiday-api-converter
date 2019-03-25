18b6ff2b1326f524866cc6136f6c01ce45b565d6
natz92 <natz.galaxy@gmail.com>
2019-03-25 21:25:13 +0900

    소스 개선

--- Convert.php
+++ Convert.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'vendor/autoload.php';
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
 class Convert
 {
@@ -8,27 +9,51 @@ class Convert
     private $serviceKey;
     private $year;
 
-    public function __construct(String $serviceKey, String $year)
+    /**
+     * Convert constructor.
+     * @param String $serviceKey
+     * @param String $year
+     * @throws Exception
+     */
+    public function __construct(String $serviceKey = null, String $year = null)
     {
         $this->year = $year;
         $this->serviceKey = $serviceKey;
 
+        $this->validate();
         $this->get();
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function validate()
+    {
+        if (empty($this->serviceKey)) {
+            throw new Exception('API 서비스키가 필요합니다');
+        }
+
+        if ($this->year < 2015) {
+            throw new Exception('2015년 이후 데이터부터 지원합니다');
+        }
     }
 
     private function get()
     {
 
-        $client = new \GuzzleHttp\Client();
+        $client = new Client();
 
         $month = 1;
-
         $text = "";
 
         while (true) {
 
             $solMonth = $month < 10 ? (int) '0' . $month : $month;
-            $response = $client->request('GET', self::SERVICE_URL . "?ServiceKey={$this->serviceKey}&solYear={$this->year}&solMonth={$solMonth}");
+            try {
+                $response = $client->request('GET', self::SERVICE_URL . "?ServiceKey={$this->serviceKey}&solYear={$this->year}&solMonth={$solMonth}");
+            } catch (GuzzleException $e) {
+                echo $e->getMessage();
+            }
 
             $xml = simplexml_load_string($response->getBody()->getContents());
 
@@ -57,6 +82,11 @@ class Convert
 
         echo $this->year . "년 생성 완료";
     }
+
 }
 
-$script = new Convert($argv[1], $argv[2]);
+try {
+    $script = new Convert($argv[1], $argv[2]);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
